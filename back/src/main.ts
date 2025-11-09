d9bf05c30dad6ee16751b41e37972c4871db21c2
Vitaly Frolov <vifrolov@rdcenter.ru>
2025-11-09 23:21:00 +0300

    fix(CORS)

--- back/src/main.ts
+++ back/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
   // Включаем CORS с правильной конфигурацией
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    origin: ['http://95.174.104.37:3000'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
